eab8da50492dbb0a7f8150e7ef2aff511c5f8127
karimliaghabala <agabala.93@gmail.com>
2025-12-06 06:34:34 +0100

    x

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 
-
+const db = require('./config/database.js')
 const port = process.env.PORT||8080
 
 app.get("/",(req,res)=>{
     res.send("Salam dünya")
 })
+
+db.authenticate()
+  .then(() => {
+    console.log("✅ Neon-a bağlandı")
+  })
+  .catch((err) => {
+    console.error("❌ DB Xətası:", err.message)
+  })
 
 app.listen(port,()=>{
     `Server ${port} da bashladildi`
