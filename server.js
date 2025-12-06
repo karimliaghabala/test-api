52c6de6c9dcc4c8ee8bc3179127c8cb93c281f51
karimliaghabala <agabala.93@gmail.com>
2025-12-06 06:12:40 +0100

    added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT||8080
+
+app.get("/",(req,res)=>{
+    res.send("Salam dünya")
+})
+
+app.listen(port,()=>{
+    `Server ${port} da bashladildi`
+})
